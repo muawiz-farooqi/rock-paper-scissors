ae127305e03c0fad4ba83b9b5f261b7711baaf00
Muhammad Muawiz Farooqi <muawizfarooqi@yahoo.com>
2026-09-07 19:00:58 +0400

    added boilerplate script.js linked to empty html

---
+++ script.js
@@ -0,0 +1,13 @@
+// where is round is played and decided. should return the winner or the updated scores
+function playRound()
+{
+
+}
+
+// where the game is played
+function game()
+{
+
+}
+
+// where the game starts. also handles cancel or restart
